e01e7d983a4927897ae96e21867a8d2fe3a35f0a
erikaosterbur <erikaosterbur@gmail.com>
2022-01-10 19:06:44 -0600

    updated OO countdown

--- src/components/CountdownOO.jsx
+++ src/components/CountdownOO.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 function CountdownOO(){
     const calculateTimeLeft = () => {
-        const difference = +new Date(`01/31/2022 16:55`) - +new Date();
+        const difference = +new Date(`01/30/2022 12:45`) - +new Date();
         let timeLeft = {};
         if (difference > 0) {
             timeLeft = {
